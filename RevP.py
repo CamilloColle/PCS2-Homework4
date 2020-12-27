f383ed752a616902edf1eafc78dcbe1ff8e12c77
CamilloColle <72974010+CamilloColle@users.noreply.github.com>
2020-12-27 19:09:11 +0100

    Update RevP.py

--- RevP.py
+++ RevP.py
@@ -5,7 +5,11 @@ records = list(SeqIO.parse("rosalind_revp.txt", "fasta"))
 S = records[0].seq
 
 
-for i in range(len(S)):
-    for j in range(4, 13):
-        if S[i:i+j]==reverse_complement(S[i:i+j]) and i+j <= len(S):
-            print(i+1, j)
+def revp():
+    for i in range(len(S)):
+        for j in range(4, 13):
+            if S[i:i+j]==reverse_complement(S[i:i+j]) and i+j <= len(S):
+                print(i+1, j)
+
+
+revp()
